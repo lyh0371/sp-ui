09a55ea8f6f66a57e542f828db5a6196ac9784d4
liuyahui <1347635797@qq.com>
2022-08-18 09:46:46 +0800

    🚀perf(button): 测试

---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,3 @@
+<template>你好 我是button</template>
+<script lang="ts" setup></script>
+<style scoped></style>
---
+++ play/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Button></Button>
+</template>
+<script lang="ts" setup>
+import Button from '../../packages/components/button'
+</script>
+<style scoped></style>
